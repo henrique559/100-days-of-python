36a889a194f64118f586641c68e806d4837b7844
iwakurasorcerer <145483144+henrique559@users.noreply.github.com>
2023-12-06 20:35:05 -0300

    Create randomization.py

---
+++ Day 4/randomization.py
@@ -0,0 +1,12 @@
+import random 
+# import my_module
+
+random_integer = random.randint(1, 5)
+print(f"INT: {random_integer}")
+# print(my_module.pi)
+
+#0.00000000 - 0.9999999
+random_float = random.random()
+print(f'FLOAT: {format(float(random_integer) * random_float, ".1f")}')
+love_score = random.randint(1,100)
+print(f"Your love score is {love_score}")[
